c060f5f0f7efd6358b86b75e3015a5da851326ff
unknown <mohit.joshi@coditas.com>
2024-06-16 17:40:30 +0530

    added flask application

---
+++ Production-level-flask-application/tests/test_1.py
@@ -0,0 +1,26 @@
+from server import create_app
+import pytest
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_request_example(client):
+    response = client.get('/query/greeting')
+    assert b'Hello this message is coming from query Blueprint.' in response.data
